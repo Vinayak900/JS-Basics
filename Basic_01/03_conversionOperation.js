007ae38faab7bb81932bf112de266cc2c50819f3
Vinayak900 <140071699+Vinayak900@users.noreply.github.com>
2024-09-26 17:32:23 +0000

    converstion

---
+++ Basic_01/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score =33
+let b=String(score)
+console.log(typeof b);
+
+let a="123"
+let c=Number(a);
+console.log(typeof c);
+       //converting to Number and respective outputs
+//"33" => 33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//null=>0
+//undefined=>Nan
+  //Converting to Bool
+//""=>false
+//0=>false
+//1=>true
+//"abc"=>true
+   //converting to String
+ //123=>"123"
+ //null=>null
+ 
