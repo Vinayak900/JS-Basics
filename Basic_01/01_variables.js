c49edf3824def449dc29ad78626bb5ca906cae1c
Vinayak900 <140071699+Vinayak900@users.noreply.github.com>
2024-09-26 16:57:43 +0000

    variables and datatypes

---
+++ Basic_01/01_variables.js
@@ -0,0 +1,11 @@
+const acoountId=12;
+let email='abc@gmail.com';
+var pass='12345'
+city='bang'
+// acoountId=2        //We cannot re assign value to const
+// prepor not to use var variable because of function and block scope
+console.log(acoountId);
+console.log(email);
+console.log(pass);
+console.log(typeof(city));
+console.table([acoountId,email,pass,city])
---
+++ Basic_01/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS code as newer version
+
+// TC39 and mdn documentation
+
+               // datatypes
+   // Primitive
+//1.number => 2 to the power of 53
+//2.bigint
+//3.string =>"" prefer always double quotes
+//4.boolean
+//5.null => standalone value
+//6.undefined => value not assigned
+//7.symbol => to find uniqueness 
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
