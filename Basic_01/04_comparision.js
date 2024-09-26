d280e4543154185a7db0df7e1a9b1e7c1aee6fab
Vinayak900 <140071699+Vinayak900@users.noreply.github.com>
2024-09-26 17:51:19 +0000

    comparision

---
+++ Basic_01/04_comparision.js
@@ -0,0 +1,6 @@
+//whenever you want to compare two diff datatypes concentarte on datatypes 
+
+console.log("2"==2);//it will compare values and JS start converting string to number
+
+console.log("2"===2);//it will compare both value and datatype
+
